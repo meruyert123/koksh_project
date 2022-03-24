f65d4bfe2b2b7b45c9069deef90ce072cc1dcd19
Diana <200103329@stu.sdu.edu.kz>
2022-03-24 17:41:38 +0600

    vi

--- poll/views.py
+++ poll/views.py
@@ -1,19 +1,12 @@
-<<<<<<< HEAD
 from django.http import HttpResponse
-=======
-from django.http import HttpRequest
->>>>>>> 009f8567d3dc6cb741e04fb6e53207dcb1b8a473
 from django.shortcuts import render
 
 # Create your views here.
 
-<<<<<<< HEAD
 def iambest(request):
     return HttpResponse('HELLO')
-=======
 
 def didi(request):
     print('fifa')
 
     
->>>>>>> 009f8567d3dc6cb741e04fb6e53207dcb1b8a473
